b17cbe1658e4e3b7a20aff9868966ed6b175e0ad
anita0402 <33681420+anita0402@users.noreply.github.com>
2017-11-15 15:20:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,34 +7,20 @@
   <body>
 
     
-    <?php
-	$now = getdate(time());
-	$time = mktime(0,0,0, $now['mon'], 1, $now['year']);
-	$date = getdate($time);
-	$dayTotal = cal_days_in_month(0, $date['mon'], $date['year']);
-	//Print the calendar header with the month name.
-	print '<table><tr><td colspan="7"><strong>' . $date['month'] . '</strong></td></tr>';
-	for ($i = 0; $i < 6; $i++) {
-		print '<tr>';
-		for ($j = 1; $j <= 7; $j++) {
-			$dayNum = $j + $i*7 - $date['wday'];
-			//Print a cell with the day number in it.  If it is today, highlight it.
-			print '<td';
-			if ($dayNum > 0 && $dayNum <= $dayTotal) {
-				print ($dayNum == $now['mday']) ? ' style="background: #ccc;">' : '>';
-				print $dayNum;
-			}
-			else {
-				//Print a blank cell if no date falls on that day, but the row is unfinished.
-				print '>';
-			}
-			print '</td>';
-		}
-		print '</tr>';
-		if ($dayNum >= $dayTotal && $i != 6)
-			break;
-	}
-	print '</table>';
-?>
+    
+value of a:<input id="first" type="text" onchange="addition()"  />
+value of b:<input id="second" type="text" onchange="addition()"  />
+value of c:<input id="result" type="text" />
+
+<div id="div"></div>
+<script>
+function addition()
+{
+var a = parseInt(document.getElementById('first').value);
+var b = parseInt(document.getElementById('second').value);
+ var c = a + b;
+document.getElementById('result').value=c;
+}
+</script>
   </body>
 </html>
